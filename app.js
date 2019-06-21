a77c2e68dc999247b7bd26b2e0a5e84a1a71b807
Matt Jackson <MatthewP.Jackson@churchofJesusChrist.orgCreate repositoryImport repositoryCreate snippetManage account>
2019-06-21 09:05:32 -0600

    added array methods to js file

--- app.js
+++ app.js
@@ -11,3 +11,131 @@ const companies = [
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// FOR LOOP
+// for (let i = 0; i < companies.length; i++) {
+//   console.log(companies[i]);
+// }
+
+/*
+======================
+FOREACH
+======================
+*/
+
+// companies.forEach(function(company) {
+//   console.log(company.name);
+// });
+
+// companies.forEach(company => console.log(company.name));
+
+/*
+======================
+FILTER
+======================
+*/
+
+// FILTER AGES OVER 21
+// const overTwentyOne = ages.filter(function(age) {
+//   if (age >= 21) {
+//     return true;
+//   }
+// });
+
+const overTwentyOne = ages.filter(age => age >= 21);
+
+// FILTER RETAIL COMPANIES
+const isRetail = companies.filter(company => company.category === "Retail");
+
+// FILTER ALL COMPANIES FROM THE 80'S
+const eightiesCompanies = companies.filter(
+  company => company.start > 1970 && company.start < 1990
+);
+
+// FILTER ALL COMPANIES THAT LASTED MORE THAN 20 YEARS
+// const overTwenty = companies.filter(function(company) {
+//   if (company.end - company.start >= 20) {
+//     return true;
+//   }
+// });
+
+const overTwenty = companies.filter(
+  company => company.end - company.start >= 20
+);
+
+/*
+======================
+MAP
+======================
+*/
+
+// CREATE ARRAY OF COMPANY NAMES
+// const companyNames = companies.map(function(company) {
+//   return company.name;
+// });
+
+const companyNames = companies.map(company => company.name);
+
+// CREATE TEST MAP ARRAY
+const testMap = companies.map(
+  company => `${company.name} [${company.start} - ${company.end}]`
+);
+
+// CREATE AN ARRAY WITH THE SQUARE ROOT OF EACH NUMBER
+const ageSquare = ages.map(age => Math.sqrt(age));
+
+// CREATE A NEW ARRAY WITH THE SQUARE ROOF OF EACH NUMBER
+// AND THEN CREATE A NEW ARRAY WITH THE NEW NUMBER TIMES TWO
+const timesTwo = ages.map(age => Math.sqrt(age)).map(age => age * 2);
+
+/*
+======================
+SORT
+======================
+*/
+
+// SORT COMPANIES BY START YEAR
+// const sortedCompanies = companies.sort(function(c1, c2) {
+//   if (c1.start > c2.start) {
+//     return 1;
+//   } else {
+//     return -1;
+//   }
+// });
+
+const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
+
+// SORT AGES
+const sortAges = ages.sort((a, b) => (a > b ? 1 : -1));
+
+/*
+======================
+REDUCE
+======================
+*/
+
+// ADD ALL AGES TOGETHER
+// const ageSum = ages.reduce(function(total, age) {
+//   return total + age;
+// }, 0);
+
+const ageSum = ages.reduce((total, age) => total + age, 0);
+
+// TOTAL YEARS FOR ALL COMPANIES
+const totalYears = companies.reduce(
+  (total, company) => total + (company.end - company.start),
+  0
+);
+
+/*
+======================
+COMBINE METHODS
+======================
+*/
+
+const combined = ages
+  .map(age => age * 2)
+  .filter(age => age >= 40)
+  .sort((a, b) => a - b)
+  .reduce((a, b) => a + b, 0);
+console.log(combined);
